01eaf533f5ade751ddc1f868f7c54940de9ee433
csjeong <bluelabels78@gmail.com>
2023-03-13 20:52:20 +0900

    이미지 가져오기

--- src/renderer/hello/Hello.tsx
+++ src/renderer/hello/Hello.tsx
@@ -1,3 +1,28 @@
+import DogJpgImage from './assets/dog.jpg';
+import DogPngImage from './assets/dog.png';
+import DogSvgImageUrl, {
+  ReactComponent as DogSvgImage,
+} from './assets/dog.svg';
+
 export default function Hello() {
-  return <div>Empty content</div>;
+  return (
+    <div className="flex">
+      <div>
+        <h1 className="text-3xl">dog.jpg</h1>
+        <img src={DogJpgImage} alt="dog" className="h-[400px] w-fit" />
+      </div>
+      <div>
+        <h1 className="text-3xl">dog.png</h1>
+        <img src={DogPngImage} alt="dog" className="h-[400px] w-fit" />
+      </div>
+      <div>
+        <h1 className="text-3xl">dog.svg</h1>
+        <img src={DogSvgImageUrl} alt="dog" className="h-[400px] w-fit" />
+      </div>
+      <div>
+        <h1 className="text-3xl">dog.svg as ReactComponent</h1>
+        <DogSvgImage className="h-[300px] w-fit" />
+      </div>
+    </div>
+  );
 }
